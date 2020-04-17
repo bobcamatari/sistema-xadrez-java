b3d1668536c5244af4d1a03791561b071e2a1574
bobcamatari <bob.365consultoria@gmail.com>
2020-04-17 18:43:33 -0300

    Implementado Tabuleiro e peças

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,12 +1,13 @@
 package aplicacao;
 
 import tabuleiroJogo.Posicao;
+import tabuleiroJogo.Tabuleiro;
 
 public class Programa {
 
 	public static void main(String[] args) {
-		Posicao posicao = new Posicao(3, 5);
-		System.out.println(posicao);
+		
+		Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 				
 
 	}
